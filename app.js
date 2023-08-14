d2f308b098b4b863c030af82d7178c768df51e33
Mahedee Hasan <mahedee007@gmail.com>
2023-08-14 16:16:05 +0600

    more functionality added

--- app.js
+++ app.js
@@ -21,3 +21,29 @@ document.getElementById('tri-btn').addEventListener('click', function(){
     <li><h5>Triangle</> ${area} cm <sup>2</sup> <button class="btn btn-primary">covert</button></li>
   </ol>`
 })
+
+document.getElementById('ract-btn').addEventListener('click', function(){
+    const ractW = inputs('ract-w');
+    const ractH = inputs('ract-h');
+    const area = (ractW * ractH).toFixed(2);
+    // const calculateTotal = document.getElementById('display');
+    // const ol = document.createElement('ol');
+    // ol.innerHTML = ` <li> ${area} cm<sup>2</sup> <button class="btn btn-primary">Convert</button>`
+    // calculateTotal.appendChild(ol);
+    // Create a new list item for the result
+    const resultItem = document.createElement('li');
+
+    // Create an <h3> tag for the area value
+    const areaHeader = document.createElement('h5');
+    areaHeader.textContent = `Triangle Area: ${area}`;
+    
+    // Create a button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    resultItem.appendChild(areaHeader);
+    resultItem.appendChild(deleteButton);
+    
+    const resultList = document.getElementById('result-list');
+    resultList.appendChild(resultItem);
+  
+})
