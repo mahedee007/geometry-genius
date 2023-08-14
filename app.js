3483b69bac394aeb9c34463a6170d2d54b055ef5
Mahedee Hasan <mahedee007@gmail.com>
2023-08-14 13:36:49 +0600

    function added

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+
+function inputs(id){
+    const input = document.getElementById(id);
+    const inputString = input.value;
+    const inputNumber = parseFloat(inputString);
+    input.value = '';
+    return inputNumber;
+    
+}
+
+
+
+
+
 document.getElementById('tri-btn').addEventListener('click', function(){
-    const triangleWidth = document.getElementById('tri-w');
-    const triangleWidthString = triangleWidth.value;
-    const triangleWidthNumber = parseFloat(triangleWidthString);
-    const triangleHeight = document.getElementById('tri-h');
-    const triangleHeightString = triangleHeight.value;
-    const triangleHeightNumber = parseFloat(triangleHeightString);
-    const area = (0.5 * triangleWidthNumber * triangleHeightNumber).toFixed(2);
-    triangleWidth.value = '';
-    triangleHeight.value = '';
+    const triangleWidth = inputs('tri-w');
+    const triangleHeight = inputs('tri-h');
+    const area = (0.5 * triangleWidth * triangleHeight).toFixed(2);
     const calculateTotal = document.getElementById('display');
     calculateTotal.innerHTML = `<ol>
     <li><h5>Triangle</> ${area} cm <sup>2</sup> <button class="btn btn-primary">covert</button></li>
